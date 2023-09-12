037874a6ce1f04946816838322f72ad5dd7471b2
khakimdon <almamedovkh@mail.ru>
2023-09-12 18:05:44 +0500

    first commit

---
+++ src/components/Brand.vue
@@ -0,0 +1,65 @@
+<script setup>
+
+</script>
+
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="Brand min-h-[355px] mb-[100px] w-[100%] text-[#2A254B]">
+        <div class="h-[100%] w-[90%] m-auto">
+            <h2 class="text-center pt-[60px] text-[24px]">What makes our brand different?</h2>
+            
+            <div class="brandWrp h-[max-content] w-[100%] mt-[51px] flex flex-wrap justify-between items-start">
+                
+                <div class="h-[100%] w-[600px] flex flex-col mb-[50px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
+</svg>
+
+                    <span class="text-[20px] mt-[16px]">integrated Approach:</span>
+                    <p class="mt-[7px]"> Unlike many solar energy companies, we offer a comprehensive approach that spans education, innovation, and execution. Our Solar Energy Academy equips individuals with practical and in-demand solar energy skills, ensuring they are well-prepared for the industry. Meanwhile, our monthly seminars, "Solar Club," keep everyone updated on the latest trends and innovations in the field.</p>
+
+                </div>
+
+                <div class="h-[100%] w-[600px] flex flex-col mb-[50px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+</svg>
+
+                    <span class="text-[20px] mt-[16px]"> Practical Expertise: </span>
+                    <p class="mt-[7px]"> We understand the importance of practical knowledge. Our academy doesn't just focus on theory; we bring in experts with hands-on experience as instructors. This means our students gain not only knowledge but also practical skills, making them highly competitive in the job market.</p>
+
+                </div>
+                
+                <div class="h-[100%] w-[600px] flex flex-col mb-[50px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+</svg>
+
+                    <span class="text-[20px] mt-[16px]"> Career Opportunities: </span>
+                    <p class="mt-[7px]"> We go beyond education. Upon graduation, we offer our students the opportunity to join our executive team, depending on their location and the project. This unique approach not only fosters growth within our organization but also strengthens the solar energy community.</p>
+
+                </div>
+
+                <div class="h-[100%] w-[600px] flex flex-col mb-[50px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
+</svg>
+
+                    <span class="text-[20px] mt-[16px]">  End-to-End Solutions: </span>
+                    <p class="mt-[7px]"> From education to execution, we cover it all. Not only do we offer training and seminars, but we're also a solar power plant company. We design and implement solar power projects across Uzbekistan, serving industrial, household, and agricultural needs. Plus, we provide high-quality solar energy devices, ensuring our customers have access to top-notch equipment for their energy needs.</p>
+
+                </div>
+
+              
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style></style>
---
+++ src/components/Content.vue
@@ -0,0 +1,75 @@
+<script setup>
+</script>
+
+<script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+// Import Swiper styles
+import 'swiper/css';
+
+import 'swiper/css/pagination';
+
+import '../swiper.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+export default {
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        return {
+            modules: [Pagination],
+        };
+    },
+    
+};
+</script>
+
+
+<template>
+    <div class="slid content h-[657px] w-[100%]">
+        <div class="h-[100%] w-[90%] m-auto flex flex-col text-[#22343D]">
+            <h2 class="text-[34px] font-[700]">{{ $t("education.content") }}</h2>
+            <p class="mt-[18px] font-[500]">{{ $t("education.focusAway") }} </p>
+
+            <div class="h-[385px] w-[100%] flex mt-[33px] justify-between">
+
+                <swiper :slidesPerView="3"  :pagination="{
+                    clickable: true,
+                }" :modules="modules" class="mySwiper">
+
+                    <swiper-slide v-for="item of 8" :key="item.id">
+                        <div class="h-[250px] w-[350px] flex flex-col">
+                            <img :src="`src/assets/slideslider/${item}.jpg`" alt="">
+                        </div>
+                    </swiper-slide>
+                   
+                    
+                </swiper>
+
+
+
+
+
+
+
+
+            </div>
+
+            <!-- <div class="h-[20px] w-[100%] bg-[#979797] mt-[50px]"></div> -->
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@media screen and (max-width: 500px) {
+    .slid{
+        display: none !important;
+    }
+}
+</style>
